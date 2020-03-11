8702e747171f99a1f75f50583221f2f1feb9f555
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-11 23:30:48 +0300

    Add association between the event attendee (also a User) and the event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :events
+  has_many :event_attendees
+  has_many :attended_event, class_name: "Event", through: :event_attendees
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_153327) do
+ActiveRecord::Schema.define(version: 2020_03_11_202453) do
+
+  create_table "event_attendees", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_attendees_on_event_id"
+    t.index ["user_id"], name: "index_event_attendees_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
