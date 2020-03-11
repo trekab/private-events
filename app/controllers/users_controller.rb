9066b94e9f940bf35cc1e91073fc8e3823ff3be5
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-11 18:59:50 +0300

    Modify the User’s Show page to list all the events a user has created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_show_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
   end
 
   def show
-    @users = User.all
+    @user = User.find(session[:user_id])
   end
 
   private
