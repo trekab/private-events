784ce333bedd10d775bf740cd61e03054c9a8abd
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-11 19:23:59 +0300

    Add an EventsController and corresponding routes to create an event, show a single event, and list all events

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'events/new', to: 'events#new'
+  post 'events/new', to: 'events#create'
+  get 'events/index', to: 'events#index'
+  get 'events/:id', to: 'events#show'
 end
