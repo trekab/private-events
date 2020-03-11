8d356879f8da9f3b8d39efc7f3782fdb2b8e0bab
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-11 16:17:35 +0300

    Add login form

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   post 'users/new', to: 'users#create'
   get 'users/create'
   get 'users/show'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#new'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
