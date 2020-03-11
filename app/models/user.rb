14be810b2601f03505e34fee416170cf4510dce5
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-12 00:36:46 +0300

    Modify the User’s Show page to separate events which have occurred in the past (“Previously attended events”) from those which are occurring in the future (“Upcoming events”)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   has_many :event_attendees
   has_many :attended_event, class_name: "Event", through: :event_attendees
   has_secure_password
+
+  def upcoming_events
+    self.attended_event.where("date > ?", Time.now)
+  end
+
+  def previous_events
+    self.attended_event.where("date < ?", Time.now)
+  end
 end
