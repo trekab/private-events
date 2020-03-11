eedf396fe05943c759b13799ad15f1195a995621
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-11 16:52:22 +0300

    Build and migrate Event model without any foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_013012) do
+ActiveRecord::Schema.define(version: 2020_03_11_135048) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
