f10f32cad50ab6f7a634720062dfc8779a29a3e7
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-11 16:47:27 +0300

    Create a simple sign in function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    debugger
+    user = User.find_by(id: params[:session][:id])
+    if user
+      session[:user_id] = user.id
+      redirect_to users_show_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'users/create'
   get 'users/show'
   get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
